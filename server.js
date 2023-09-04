627e7b3048ee0a44fa8cc6b1aa12ee081059ede4
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-09-04 18:09:59 +0200

    connect mongoose

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+const mongoose = require('mongoose')
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000')
+const DB_HOST = 'mongodb+srv://Tetiana:Y5w!!Wcngcd4uwN@cluster0.nnfnnfc.mongodb.net/contacts_reader?retryWrites=true&w=majority'
+
+mongoose.set('strictQuery', true)
+mongoose.connect(DB_HOST)
+  .then(()=> {
+    app.listen(3000, () => {
+    console.log('Database connection successful')
+  })
 })
+  .catch(error => {
+    console.log(error.message)
+    // команда, яка закриває запущені процеси (1- закрити з невідомою помилкою)
+    process.exit(1) 
+  })
+
+
+
