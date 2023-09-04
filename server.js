51e65fa9ae36ab5c3a78efe914b5ab8a42f65176
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-09-04 19:08:54 +0200

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose')
 
 const app = require('./app')
 
-const DB_HOST = 'mongodb+srv://Tetiana:Y5w!!Wcngcd4uwN@cluster0.nnfnnfc.mongodb.net/contacts_reader?retryWrites=true&w=majority'
+// const DB_HOST = 'mongodb+srv://Tetiana:Y5w!!Wcngcd4uwN@cluster0.nnfnnfc.mongodb.net/contacts_reader?retryWrites=true&w=majority'
+// const {DB_HOST} = require('./config')
+const {DB_HOST} = process.env
 
+console.log(process.env)
 mongoose.set('strictQuery', true)
 mongoose.connect(DB_HOST)
   .then(()=> {
     app.listen(3000, () => {
-    console.log('Database connection successful')
+    console.log('Database connection successful!!')
   })
 })
   .catch(error => {
