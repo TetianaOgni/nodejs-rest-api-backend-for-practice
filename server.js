c0c636177e7c67aa0e558ce2a72ff2b948eb0afd
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-09-06 12:43:50 +0200

    use process.env

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose')
 
 const app = require('./app')
-
-// const DB_HOST = 'mongodb+srv://Tetiana:Y5w!!Wcngcd4uwN@cluster0.nnfnnfc.mongodb.net/contacts_reader?retryWrites=true&w=majority'
-// const {DB_HOST} = require('./config')
-const {DB_HOST} = process.env
-
-console.log(process.env)
-mongoose.set('strictQuery', true)
-mongoose.connect(DB_HOST)
-  .then(()=> {
+// mongodb+srv://Tetiana:Y5w!!Wcngcd4uwN@cluster0.nnfnnfc.mongodb.net/contacts_reader?retryWrites=true&w=majority
+// const {DB_HOST} = process.env
+// mongoose.set('strictQuery', true)
+mongoose.connect(process.env.DB_HOST)
+  .then(() => {
     app.listen(3000, () => {
     console.log('Database connection successful!!')
   })
