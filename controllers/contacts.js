a2995d92b63c5d0761597a5f85fb89177748f86c
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-09-08 18:42:13 +0200

    delete all commented code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,24 +1,12 @@
 const {Contact} = require('../models/Contact.js')
-// const contacts = require('../models/contacts')
 const {HttpError, ctrlWrapper }= require('../helpers')
 
-  // const listContacts = async (req, res, next) => {
-  //     const result = await contacts.listContacts()
-  //     res.json(result)
-  // }
+ 
   const listContacts = async (req, res) => {
     const result = await Contact.find()
     res.json(result)
 }
 
-  // const getContactById = async (req, res, next) => {
-  //   const {id} = req.params
-  //   const result = await contacts.getContactById(id)
-  //   if (!result){
-  //     throw HttpError(404, 'Not found')
-  //   }
-  //   res.json(result) 
-  // }
   const getContactById = async (req, res, next) => {
     const {id} = req.params
     const result = await Contact.findById(id)
@@ -28,25 +16,12 @@ const {HttpError, ctrlWrapper }= require('../helpers')
     res.json(result) 
   }
 
-  // const addContact = async (req, res, next) => {
-  //     const result = await contacts.addContact(req.body)
-  //     res.status(201).json(result)
-  // }
+ 
   const addContact = async (req, res, next) => {
     const result = await Contact.create(req.body)
     res.status(201).json(result)
 }
 
-  // const removeContact = async (req, res, next) => {
-  //     const {id} = req.params
-  //     const result = await contacts.removeContact(id)
-  //     if (!result){
-  //       throw HttpError(404, 'Not found')
-  //     }
-  //     res.json({
-  //       message: 'contact delete'
-  //     })
-  // }
   const removeContact = async (req, res, next) => {
     const {id} = req.params
     const result = await Contact.findByIdAndRemove (id)
@@ -58,15 +33,6 @@ const {HttpError, ctrlWrapper }= require('../helpers')
     })
 }
 
-  // const updateContact = async (req, res, next) => {
-  //       const {id} = req.params
-  //       const data= req.body
-  //       const result = await contacts.updateContact(id, data)
-  //       if (!result){
-  //         throw HttpError(404, 'Not found')
-  //       }
-  //       res.json(result) 
-  // }
   const updateContact = async (req, res, next) => {
     const {id} = req.params
     const data= req.body
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 const app = require('./app')
-// mongodb+srv://Tetiana:Y5w!!Wcngcd4uwN@cluster0.nnfnnfc.mongodb.net/contacts_reader?retryWrites=true&w=majority
-// const {DB_HOST} = process.env
+
 mongoose.set('strictQuery', true)
 const {DB_HOST, PORT = 3000} = process.env
 mongoose.connect(DB_HOST)
@@ -13,7 +12,6 @@ mongoose.connect(DB_HOST)
 })
   .catch(error => {
     console.log(error.message)
-    // команда, яка закриває запущені процеси (1- закрити з невідомою помилкою)
     process.exit(1) 
   })
 
