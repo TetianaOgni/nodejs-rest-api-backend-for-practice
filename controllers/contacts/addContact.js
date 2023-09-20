a7d651a4179d83c4a6ce41a5892b8e44673b9cbf
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-09-20 18:17:06 +0200

    create link on file and create middleware for static files

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,12 +1,18 @@
+const fs = require('fs/promises')//модуль FileSystem отвечает за работу с файлами в Node.js
+const path = require('path')//   модуль path, який входить до стандартної бібліотеки Node.js потрібен роботи з шляхами файлової системи
+
 const {Contact}= require('../../models/Contact')
 const {ctrlWrapper }= require('../../helpers')
+
+const avatarsPath = path.resolve('public', 'avatars') 
 const addContact = async (req, res, next) => {
     const {_id: owner} = req.user
-
-    console.log(req.body)
-    console.log(req.file)
-//     const result = await Contact.create({...req.body, owner})
-//     res.status(201).json(result)
+    const {path: oldPath, filename} = req.file 
+    const newPath = path.join(avatarsPath, filename) 
+    await fs.rename(oldPath, newPath)
+    const avatar = path.join('avatars', filename)
+    const result = await Contact.create({...req.body, avatar, owner})
+    res.status(201).json(result)
 }
 
 module.exports = {
